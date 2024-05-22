b6ed0fdb7a57d6cbd59785aaf11638b63d522aa3
jasmin <chjasmin27@gmail.com>
2024-05-22 09:17:26 +0600

    root route

--- src/app.ts
+++ src/app.ts
@@ -10,12 +10,16 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/", router);
-app.use((req, res,) => {
-  res.status(404).json({ success: false, message: "Route not found" });
-});
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello from server");
+  res.status(200).json({
+    success : true,
+    message : "welcome!! The server is online",
+  });
+});
+app.use((req, res, next) => {
+  res.status(404).json({ success: false, message: "Route not found" });
+  next()
 });
 
 export default app;
